4469b8a8a8bd21370d42dd6499075f83616e50ed
tomokawatakumi <tomokawa@studio9.team>
2020-06-08 21:35:52 +0900

    Local initial commit

---
+++ InstagramClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramClone
+//
+//  Created by Tomokawa Takumi on 2020/06/08.
+//  Copyright © 2020 apex. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
